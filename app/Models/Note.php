ee7a240e1d5bc6e9d1fb4c80ea9194070eadd59a
Arushad Faisal <arushadfaisal@gmail.com>
2026-08-27 18:59:01 +0500

    fix bug Note model. missing protected

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    $fillable = ['title', 'content'];
+    protected $fillable = [
+        'title', 
+        'content'
+    ];
 }
